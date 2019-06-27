cbcb5c6c7a38c204b7d4c806923dcec58e7f56a1
Rahim <rahim.laiwalla@gmail.com>
2019-06-26 19:27:13 -0700

    testbuilder complete

--- detectNetwork.js
+++ detectNetwork.js
@@ -10,7 +10,10 @@
 var detectNetwork = function(cardNumber) {
   // Note: `cardNumber` will always be a string
   var firstTwoDigits = cardNumber.slice(0,2)
-  var firstFourDigits = cardNumber.slice(0,4)
+  var first3 = Number(cardNumber.slice(0,3))
+  var first4 = cardNumber.slice(0,4)
+  var numFirst4 = Number(first4)
+  var first6 = Number(cardNumber.slice(0,6))
   var digitName = ''
   var lengthName = ''
   if(firstTwoDigits === '38' || firstTwoDigits === '39'){
@@ -21,26 +24,114 @@ var detectNetwork = function(cardNumber) {
     digitName = 'Visa'
   } else if(firstTwoDigits === '51' || firstTwoDigits === '52' ||firstTwoDigits === '53' || firstTwoDigits === '54' || firstTwoDigits === '55'){
     digitName = 'MasterCard'
-  } else if(firstFourDigits === '6011' && cardNumber.length === 16){
+  } else if(firstTwoDigits === '65' || first4 === '6011'){
     digitName = 'Discover'
-    console.log(digitName)
+  } else if(first4 === "5018" || first4 === "5020" || first4 === "5038" || first4 === "6304"){
+    digitName = "Maestro"
   }
+  
+  for(var i = 644; i<=649; i++){
+    if(first3 === i){
+      digitName = 'Discover'
+    }
+  }
+  
+  
+//   console.log(digitName)
 
   // Test for length
-  if(cardNumber.length === 14){
+  
+  if(firstTwoDigits === '65' && cardNumber.length === 19){
+    lengthName = 'Discover'
+  } else if(firstTwoDigits === '65' && cardNumber.length === 16){
+    lengthName = 'Discover'
+  } else if(first4 === '6011' && cardNumber.length === 19){
+    lengthName = 'Discover'
+  } else if(cardNumber[0] === '4' && cardNumber.length === 16){
+    lengthName = 'Visa'
+  } else if(first4 === '6011' && cardNumber.length === 16){
+    lengthName = 'Discover'
+  } else if(cardNumber.length === 14){
     lengthName = "Diner\'s Club"
   } else if(cardNumber.length === 15){
     lengthName = "American Express"
   } else if(cardNumber.length === 13 || cardNumber.length === 19){
     lengthName = 'Visa'
-  } else if(cardNumber[0] === '4' && cardNumber.length === 16){
-    lengthName = 'Visa'
-  } else if(firstFourDigits === '6011' && cardNumber.length === 16){
-    lengthName = 'Discover'
-    console.log(lengthName)
   } else if(cardNumber.length === 16){
     lengthName = 'MasterCard'
   }
+
+  for(var i = 644; i<=649; i++){
+    if(first3 === i && cardNumber.length === 16){
+      lengthName = 'Discover'
+      //console.log("16" + lengthName)
+    } else if(first3 === i && cardNumber.length === 19){
+      lengthName = 'Discover'
+      //console.log(19 + lengthName)
+    }
+  }
+
+  for(var i = 12; i<=19; i++){
+    if(first4 === '5018' && cardNumber.length === i){
+      lengthName = "Maestro"
+    } else if(first4 === '5020' && cardNumber.length === i){
+      lengthName = "Maestro"
+    } else if(first4 === '5038' && cardNumber.length === i){
+      lengthName = "Maestro"
+    } else if(first4 === '6304' && cardNumber.length === i){
+      lengthName = "Maestro"
+    } 
+  }  
+
+  //China UnionPay(combining both tests)
+  for(var i = 624; i<=626; i++){
+    for(var j = 16; j<= 19; j++){
+      if(first3 === i && cardNumber.length === j){
+        digitName = 'China UnionPay'
+        lengthName = 'China UnionPay'
+      }
+    }
+  }
+  for(var i = 6282; i<=6288; i++){
+    for(var j = 16; j<= 19; j++){
+      if(numFirst4 === i && cardNumber.length === j){
+        digitName = 'China UnionPay'
+        lengthName = 'China UnionPay'
+      }
+    }
+  }
+
+  for(var i = 622126; i<=622925; i++){
+    for(var j = 16; j<= 19; j++){
+      if(first6 === i && cardNumber.length === j){
+        digitName = 'China UnionPay'
+        lengthName = 'China UnionPay'
+      }
+    }
+  }
+  //Switch always has a prefix of 4903, 4905, 4911, 4936, 564182, 633110, 6333, or 6759 and a length of 16, 18, or 19.
+  var arraySwitch = [4903, 4905, 4911, 4936, 564182, 633110, 6333, 6759]
+  arraySwitch.forEach(function(prefix){
+    if(prefix === numFirst4 && cardNumber.length === 16){
+      digitName = 'Switch'
+      lengthName = 'Switch'
+    } else if(prefix === numFirst4 && cardNumber.length === 18){
+      digitName = 'Switch'
+      lengthName = 'Switch'
+    } else if(prefix === numFirst4 && cardNumber.length === 19){
+      digitName = 'Switch'
+      lengthName = 'Switch'
+    }else if(prefix === first6 && cardNumber.length === 16){
+       digitName = 'Switch'
+      lengthName = 'Switch'
+    } else if(prefix === first6 && cardNumber.length === 18){
+      digitName = 'Switch'
+      lengthName = 'Switch'
+    } else if(prefix === first6 && cardNumber.length === 19){
+      digitName = 'Switch'
+      lengthName = 'Switch'
+    }
+  })
 
   //test for equality
   if(digitName === 'Diner\'s Club' && lengthName === "Diner\'s Club"){
@@ -53,6 +144,12 @@ var detectNetwork = function(cardNumber) {
     return "MasterCard"
   } else if(digitName === 'Discover' && lengthName === 'Discover'){
     return "Discover"
+  } else if(digitName === "Maestro" && lengthName === "Maestro"){
+    return "Maestro"
+  } else if(digitName === 'China UnionPay' && lengthName === 'China UnionPay'){
+    return 'China UnionPay'
+  } else if(digitName === 'Switch' && lengthName === 'Switch'){
+    return 'Switch'
   }
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   // The American Express network always starts with a 34 or 37 and is 15 digits long
--- detectNetwork.test.js
+++ detectNetwork.test.js
@@ -125,14 +125,14 @@ describe('MasterCard', function() {
   // and should, but that's just for learning), so once you've gotten 
   // these tests to pass using should syntax, refactor your tests to 
   // use either expect or should, but not both. 
-  var should = chai.should();
+  var expect = chai.expect;
   
   it('has a prefix of 54 and a length of 16', function() {
-    detectNetwork('5412345678901234').should.equal('MasterCard');
+    expect(detectNetwork('5412345678901234')).to.equal('MasterCard');
   });
  
   it('has a prefix of 55 and a length of 16', function() {
-    detectNetwork('5512345678901234').should.equal('MasterCard');
+    expect(detectNetwork('5512345678901234')).to.equal('MasterCard');
   })
  
 });
@@ -153,12 +153,243 @@ describe('Discover', function() {
       throw new Error('Test failed')
     }
  });
-//   it('has a prefix of 6011 and a length of 19', function() {
-//     assert(detectNetwork('6011345678901234856') === 'Discover');
-//   });
+  it('has a prefix of 6011 and a length of 19', function() {
+    if(detectNetwork('6011345678901234567') !== 'Discover'){
+      throw new Error('Test failed')
+    }
+  });
+  it('has a prefix of 65 and a length of 16', function() {
+    if(detectNetwork('6511345678901234') !== 'Discover'){
+      throw new Error('Test failed')
+    }
+  });
+  it('has a prefix of 65 and a length of 19', function() {
+    if(detectNetwork('6511345678901234567') !== 'Discover'){
+      throw new Error('Test failed')
+    }
+  });
+//   for(prefix = 644; prefix <= 649; prefix++){
+//     var stringPrefix = prefix.toString()
+//     var stringCardNumber16 = stringPrefix + '5678901234567'
+
+//     it('has a prefix of ' + stringPrefix + ' and a length of 16', function() {
+//       if(detectNetwork(stringCardNumber16) !== 'Discover'){
+//         throw new Error('Test failed')
+//       }
+//     });
+//   }
+//   for(prefix = 644; prefix <= 649; prefix++){
+//     var stringPrefix = prefix.toString()
+//     var stringCardNumber19 = stringPrefix + '5678901234567890'
+
+//     it('has a prefix of ' + stringPrefix + ' and a length of 19', function() {
+//       if(detectNetwork(stringCardNumber19) !== 'Discover'){
+//         throw new Error('Test failed')
+//       }
+//     });
+//   }
+
+  for (var prefix = 644; prefix <= 649; prefix++) {
+      (function(prefix) {
+        var stringPrefix = prefix.toString()
+        var stringCardNumber16 = stringPrefix + '5678901234567'
+        var stringCardNumber19 = stringPrefix + '5678901234567890'
+            it('has a prefix of ' + prefix + ' and a length of 16', function(){
+              if(detectNetwork(stringCardNumber16) !== 'Discover'){
+                throw new Error('Test failed')
+              }
+           })
+            it('has a prefix of ' + prefix + ' and a length of 19', function(){
+              if(detectNetwork(stringCardNumber19) !== 'Discover'){
+                throw new Error('Test failed')
+              }
+            })
+      })(prefix)
+  }
+  
 });
 
 describe('Maestro', function() {
   // Write full test coverage for the Maestro card
+  var array = ['12345678', '123456789', '1234567890', '12345678901', '123456789012', '1234567890123', '12345678901234', '123456789012345']
+  
+   array.forEach(function(stringNumber){
+      var stringCardNumber = '5018' + stringNumber
+      it('has a prefix of 5018', function(){
+              if(detectNetwork(stringCardNumber) !== "Maestro"){
+                throw new Error('Test failed')
+              }
+           })
+   })
+  array.forEach(function(stringNumber){
+      var stringCardNumber = '5020' + stringNumber
+      it('has a prefix of 5020', function(){
+              if(detectNetwork(stringCardNumber) !== "Maestro"){
+                throw new Error('Test failed')
+              }
+      })
+   })
+   array.forEach(function(stringNumber){
+      var stringCardNumber = '5038' + stringNumber
+      it('has a prefix of 5038', function(){
+              if(detectNetwork(stringCardNumber) !== "Maestro"){
+                throw new Error('Test failed')
+              }
+       })
+   })
+   array.forEach(function(stringNumber){
+      var stringCardNumber = '6304' + stringNumber
+      it('has a prefix of 6304', function(){
+              if(detectNetwork(stringCardNumber) !== "Maestro"){
+                throw new Error('Test failed')
+              }
+       })
+   })
+
+
 });
 
+describe('China UnionPay', function() {
+  for (var prefix = 624; prefix <= 626; prefix++) {
+      (function(prefix) {
+        var stringPrefix = prefix.toString()
+        var stringCardNumber16 = stringPrefix + '5678901234567'
+        var stringCardNumber17 = stringPrefix + '56789012345678'
+        var stringCardNumber18 = stringPrefix + '567890123456789'
+        var stringCardNumber19 = stringPrefix + '5678901234567890'
+            it('has a prefix of ' + prefix + ' and a length of 16', function(){
+              if(detectNetwork(stringCardNumber16) !== 'China UnionPay'){
+                throw new Error('Test failed')
+              }
+           })
+           it('has a prefix of ' + prefix + ' and a length of 17', function(){
+              if(detectNetwork(stringCardNumber17) !== 'China UnionPay'){
+                throw new Error('Test failed')
+              }
+           })
+           it('has a prefix of ' + prefix + ' and a length of 18', function(){
+              if(detectNetwork(stringCardNumber18) !== 'China UnionPay'){
+                throw new Error('Test failed')
+              }
+           })
+            it('has a prefix of ' + prefix + ' and a length of 19', function(){
+              if(detectNetwork(stringCardNumber19) !== 'China UnionPay'){
+                throw new Error('Test failed')
+              }
+            })
+      })(prefix)
+  }
+  for (var prefix = 6282; prefix <= 6288; prefix++) {
+      (function(prefix) {
+        var stringPrefix = prefix.toString()
+        var stringCardNumber16 = stringPrefix + '567890123456'
+        var stringCardNumber17 = stringPrefix + '5678901234567'
+        var stringCardNumber18 = stringPrefix + '56789012345678'
+        var stringCardNumber19 = stringPrefix + '567890123456789'
+            it('has a prefix of ' + prefix + ' and a length of 16', function(){
+              if(detectNetwork(stringCardNumber16) !== 'China UnionPay'){
+                throw new Error('Test failed')
+              }
+           })
+           it('has a prefix of ' + prefix + ' and a length of 17', function(){
+              if(detectNetwork(stringCardNumber17) !== 'China UnionPay'){
+                throw new Error('Test failed')
+              }
+           })
+           it('has a prefix of ' + prefix + ' and a length of 18', function(){
+              if(detectNetwork(stringCardNumber18) !== 'China UnionPay'){
+                throw new Error('Test failed')
+              }
+           })
+            it('has a prefix of ' + prefix + ' and a length of 19', function(){
+              if(detectNetwork(stringCardNumber19) !== 'China UnionPay'){
+                throw new Error('Test failed')
+              }
+            })
+      })(prefix)
+  }
+  for (var prefix = 622126; prefix <= 622925; prefix++) {
+      (function(prefix) {
+        var stringPrefix = prefix.toString()
+        var stringCardNumber16 = stringPrefix + '5678901234'
+        var stringCardNumber17 = stringPrefix + '56789012345'
+        var stringCardNumber18 = stringPrefix + '567890123456'
+        var stringCardNumber19 = stringPrefix + '5678901234567'
+            it('has a prefix of ' + prefix + ' and a length of 16', function(){
+              if(detectNetwork(stringCardNumber16) !== 'China UnionPay'){
+                throw new Error('Test failed')
+              }
+           })
+           it('has a prefix of ' + prefix + ' and a length of 17', function(){
+              if(detectNetwork(stringCardNumber17) !== 'China UnionPay'){
+                throw new Error('Test failed')
+              }
+           })
+           it('has a prefix of ' + prefix + ' and a length of 18', function(){
+              if(detectNetwork(stringCardNumber18) !== 'China UnionPay'){
+                throw new Error('Test failed')
+              }
+           })
+            it('has a prefix of ' + prefix + ' and a length of 19', function(){
+              if(detectNetwork(stringCardNumber19) !== 'China UnionPay'){
+                throw new Error('Test failed')
+              }
+            })
+      })(prefix)
+  }
+})
+
+//Switch always has a prefix of 4903, 4905, 4911, 4936, 564182, 633110, 6333, or 6759 and a length of 16, 18, or 19.
+describe('Switch', function() {
+  var arraySwitch4nums = ['4903', '4905', '4911', '4936', '6333', '6759']
+  var arraySwitch6nums = ['564182', '633110']
+  
+  arraySwitch4nums.forEach(function(prefix){
+      var stringCardNumber = prefix + '567890123456'
+      it('has a lenght of 16', function(){
+              if(detectNetwork(stringCardNumber) !== "Switch"){
+                throw new Error('Test failed')
+              }
+       })
+   })
+   arraySwitch4nums.forEach(function(prefix){
+      var stringCardNumber = prefix + '56789012345678'
+      it('has a lenght of 18', function(){
+              if(detectNetwork(stringCardNumber) !== "Switch"){
+                throw new Error('Test failed')
+              }
+       })
+   })
+   arraySwitch4nums.forEach(function(prefix){
+      var stringCardNumber = prefix + '567890123456789'
+      it('has a lenght of 19', function(){
+              if(detectNetwork(stringCardNumber) !== "Switch"){
+                throw new Error('Test failed')
+              }
+       })
+   })
+   arraySwitch6nums.forEach(function(prefix){
+      var stringCardNumber = prefix + '5678901234'
+      it('has a lenght of 16', function(){
+              if(detectNetwork(stringCardNumber) !== "Switch"){
+                throw new Error('Test failed')
+              }
+       })
+   })
+   arraySwitch6nums.forEach(function(prefix){
+      var stringCardNumber = prefix + '567890123456'
+      it('has a lenght of 18', function(){
+              if(detectNetwork(stringCardNumber) !== "Switch"){
+                throw new Error('Test failed')
+              }
+       })
+   })
+   arraySwitch6nums.forEach(function(prefix){
+      var stringCardNumber = prefix + '5678901234567'
+      it('has a lenght of 19', function(){
+              if(detectNetwork(stringCardNumber) !== "Switch"){
+                throw new Error('Test failed')
+              }
+       })
+   })
+})
